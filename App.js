ab5e17f24beb35deb9e1cd5131213a1319d8723c
Matan Shaviro <matan3sh@gmail.com>
2021-06-06 20:27:03 +0300

    Building Button Component

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import AppText from './app/components/AppText';
+import AppButton from './app/components/AppButton';
 
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <AppText>Hello World</AppText>
+      <AppButton title='Login' onPress={() => console.log('Tapped!')} />
     </View>
   );
 }
