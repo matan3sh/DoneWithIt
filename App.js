140abc785db8b0679fd416c73d6b76047da13402
matan3sh <matan3sh@gmail.com>
2021-06-06 15:59:59 +0300

    Finish layout section and start implementing login screen and image screen

--- App.js
+++ App.js
@@ -1,20 +1,6 @@
 import React from 'react';
-import { View } from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: '#fff',
-        flex: 1,
-        flexDirection: 'row',
-      }}
-    >
-      <View
-        style={{ backgroundColor: 'dodgerblue', width: 100, height: 100 }}
-      />
-      <View style={{ backgroundColor: 'gold', width: 100, height: 100 }} />
-      <View style={{ backgroundColor: 'tomato', width: 100, height: 100 }} />
-    </View>
-  );
+  return <ViewImageScreen />;
 }
