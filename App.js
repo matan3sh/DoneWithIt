3dde5aea2b0c422f24abf8854b9bb01ccb384b62
Matan Shaviro <matan3sh@gmail.com>
2021-06-08 20:41:42 +0300

    Building the Listings Screen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import AccountScreen from './app/screens/AccountScreen';
+import ListingScreen from './app/screens/ListingScreen';
 
 export default function App() {
-  return <AccountScreen />;
+  return <ListingScreen />;
 }
