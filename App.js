30e6bba115e8cc81612250385fa6cf582946f911
matan3sh <matan3sh@gmail.com>
2021-06-09 15:01:00 +0300

    Building Login Screen

--- App.js
+++ App.js
@@ -1,26 +1,6 @@
-import React, { useState } from 'react';
-
-import Screen from './app/components/Screen';
-import AppPicker from './app/components/AppPicker';
-
-const categories = [
-  { label: 'Furniture', value: 1 },
-  { label: 'Clothing', value: 2 },
-  { label: 'Cameras', value: 3 },
-];
+import React from 'react';
+import LoginScreen from './app/screens/LoginScreen';
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
-
-  return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        items={categories}
-        icon='apps'
-        placeholder='Category'
-      />
-    </Screen>
-  );
+  return <LoginScreen />;
 }
