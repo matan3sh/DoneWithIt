a16db35f791cbd93dc40e26de3b95466287400ba
matan3sh <matan3sh@gmail.com>
2021-06-09 16:26:42 +0300

    Build Reusable Form Component with Formik and Yup

---
+++ app/components/AppFormField.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useFormikContext } from 'formik';
+
+import AppTextInput from '../components/AppTextInput';
+import ErrorMessage from '../components/ErrorMessage';
+
+const AppFormField = ({ name, ...otherProps }) => {
+  const { setFieldTouched, handleChange, errors, touched } = useFormikContext();
+
+  return (
+    <>
+      <AppTextInput
+        onBlur={() => setFieldTouched(name)}
+        onChangeText={handleChange(name)}
+        {...otherProps}
+      />
+      <ErrorMessage visible={touched[name]} error={errors[name]} />
+    </>
+  );
+};
+
+export default AppFormField;
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,36 +1,47 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Image, StyleSheet } from 'react-native';
+import * as Yup from 'yup';
 
 import Screen from '../components/Screen';
-import AppTextInput from '../components/AppTextInput';
-import AppButton from '../components/AppButton';
+import AppFormField from '../components/AppFormField';
+import SubmitButton from '../components/SubmitButton';
+import AppForm from '../components/AppForm';
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label('Email'),
+  password: Yup.string().required().min(6).label('Password'),
+});
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
   return (
     <Screen style={styles.container}>
       <Image style={styles.logo} source={require('../assets/logo-red.png')} />
-      <AppTextInput
-        autoCapitalize='none'
-        autoCorrect={false}
-        icon='email'
-        keyboardType='email-address'
-        onChangeText={(text) => setEmail(text)}
-        placeholder='Email'
-        textContentType='emailAddress'
-      />
-      <AppTextInput
-        autoCapitalize='none'
-        autoCorrect={false}
-        icon='lock'
-        onChangeText={(text) => setPassword(text)}
-        placeholder='Password'
-        secureTextEntry
-        textContentType='password'
-      />
-      <AppButton title='Login' onPress={() => console.log(email, password)} />
+
+      <AppForm
+        initialValues={{ email: '', password: '' }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField
+          autoCapitalize='none'
+          autoCorrect={false}
+          icon='email'
+          keyboardType='email-address'
+          name='email'
+          placeholder='Email'
+          textContentType='emailAddress'
+        />
+        <AppFormField
+          autoCapitalize='none'
+          autoCorrect={false}
+          icon='lock'
+          name='password'
+          placeholder='Password'
+          secureTextEntry
+          textContentType='password'
+        />
+        <SubmitButton title='Login' />
+      </AppForm>
     </Screen>
   );
 };
